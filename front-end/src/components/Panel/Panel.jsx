0c99e462729c1d2412e2960fc5664d2b40a40e0b
marcinpodgorski <podgorski.marcin93@gmail.com>
2024-05-20 11:57:45 +0200

    Added fetch and useEffect to upload data from localhost

--- front-end/src/components/Panel/Panel.jsx
+++ front-end/src/components/Panel/Panel.jsx
@@ -1,33 +1,19 @@
-import { useState } from "react";
-import { Button } from "../Button/Button";
+import { useEffect, useState } from "react";
 import { List } from "../List/List";
 import styles from "./Panel.module.css";
 
 export function Panel() {
-  const [data, setData] = useState([
-    {
-      id: 1,
-      word: "kot",
-      translation: "cat",
-      category: "noun",
-    },
-    {
-      id: 2,
-      word: "pies",
-      translation: "dog",
-      category: "noun",
-    },
-    {
-      id: 3,
-      word: "skakać",
-      translation: "jump",
-      category: "verb",
-    },
-  ]);
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:3000/words")
+      .then((res) => res.json())
+      .then((res) => {
+        setData(res);
+      });
+  }, []);
 
   return (
     <>
-      <Button>Załaduje dane</Button>
       <section className={styles.section}>
         <List data={data}></List>
       </section>
