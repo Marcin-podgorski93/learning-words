115aa2c930e64c229828b22fad81616395fa09e7
marcinpodgorski <podgorski.marcin93@gmail.com>
2024-05-20 22:19:35 +0200

    added loading when data was downloaded

--- front-end/src/components/Panel/Panel.jsx
+++ front-end/src/components/Panel/Panel.jsx
@@ -1,22 +1,30 @@
 import { useEffect, useState } from "react";
 import { List } from "../List/List";
 import styles from "./Panel.module.css";
+import { Form } from "../Form/Form";
 
 export function Panel() {
+  const [isLoading, setIsLoading] = useState(true);
   const [data, setData] = useState([]);
   useEffect(() => {
     fetch("http://localhost:3000/words")
       .then((res) => res.json())
       .then((res) => {
         setData(res);
+        setIsLoading(false);
       });
   }, []);
 
   return (
     <>
-      <section className={styles.section}>
-        <List data={data}></List>
-      </section>
+      {isLoading ? (
+        <p>Ładowanie</p>
+      ) : (
+        <section className={styles.section}>
+          <Form />
+          <List data={data}></List>
+        </section>
+      )}
     </>
   );
 }
